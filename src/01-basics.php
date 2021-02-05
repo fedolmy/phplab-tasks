ab738885c02624487a648fd4802ebb9abce75761
Olexandr <fedolmy@gmail.com>
2021-02-05 08:24:01 +0200

    fixed isLeapYear function in 01-basics.php

--- src/01-basics.php
+++ src/01-basics.php
@@ -45,7 +45,7 @@ function isLeapYear(int $year)
         throw new InvalidArgumentException('Too lower number');
     }
 
-    return $year % 4 === 0;
+    return $year % 4 === 0 && $year % 100 !== 0 || $year % 400 === 0;
 }
 
 /**
